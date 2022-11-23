50d0c64a8a9da45a4c6eb23fc76af477fa9b75f5
Dino <dothi.minhngoc@opcbiz.com>
2022-11-23 23:50:27 +0700

    ADd

--- public/script.js
+++ public/script.js
@@ -22,6 +22,7 @@ showChat.addEventListener("click", () => {
 const user = prompt("Enter your name");
 
 var peer = new Peer(undefined, {
+  path: '/peerjs',
   host: 'meet.medcares.net',
   port: 443,
   secure: true
